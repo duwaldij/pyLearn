7ccca79b931377ae49fb36a82243559b722dd0c2
Dijwol Duwal <ddijwol@gmail.com>
2025-08-06 23:38:57 +0545

    Basic beginner scripts

--- login_system.py
+++
@@ -1,9 +0,0 @@
-Username="frienky"
-Password="king"
-x=input("Enter your username: ")
-y=input("Enter your password: ")
-
-if x==Username and y==Password:
-    print("Login successful")
-else:
-    print("Username and Password does not match")
